c46627787c9d69a8e9b8aa005c9554e98f180887
Rahul khichar <khicharrahul16064014@gmail.com>
2025-08-14 05:49:47 +0530

    Added app config for configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<number>('PORT') ?? 3000;
+
+  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port http://localhost:${port}`);
+  await app.listen(port);
 }
+
 bootstrap();
